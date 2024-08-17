af19cdbaaf55f654ce81acc5db6a13f70dc9eb1e
x3noku <x3noku@gmail.com>
2024-08-18 01:33:53 +0600

    [Fix] Fix env imports and types

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -3,12 +3,11 @@
 import { fileURLToPath } from 'url';
 import createJiti from 'jiti';
 
-import { env } from './env';
-
 // Import env files to validate at build time. Use jiti so we can load .ts files in here.
-createJiti(fileURLToPath(import.meta.url))('./env');
+/** @type {import('./env.ts')} */
+const { env } = createJiti(fileURLToPath(import.meta.url))('./env');
 
-/** @type {import("next").NextConfig} */
+/** @type {import('next').NextConfig} */
 const config = {
     output: env.DOCKER ? 'standalone' : undefined,
     reactStrictMode: true,
