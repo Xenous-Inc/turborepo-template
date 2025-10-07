9959dc9dab8511c78928fa475cebc2a9d30f9879
x3noku <x3noku@gmail.com>
2025-10-07 08:42:36 +0600

    chore(server): add rpc scalar client

--- apps/server/src/routes/rpc/[...].ts
+++ apps/server/src/routes/rpc/[...].ts
@@ -1,17 +1,35 @@
+import { OpenAPIHandler } from '@orpc/openapi/fetch';
+import { OpenAPIReferencePlugin } from '@orpc/openapi/plugins';
 import { RPCHandler } from '@orpc/server/fetch';
+import { ZodToJsonSchemaConverter } from '@orpc/zod/zod4';
 import { appRouter } from '~/routers';
 
-const handler = new RPCHandler(appRouter);
+const rpcHandler = new RPCHandler(appRouter);
+
+const apiHandler = new OpenAPIHandler(appRouter, {
+    plugins: [
+        new OpenAPIReferencePlugin({
+            schemaConverters: [new ZodToJsonSchemaConverter()],
+        }),
+    ],
+});
 
 export default defineEventHandler(async event => {
     const context = await createContext({ event });
 
     const request = toWebRequest(event);
 
-    const { matched, response } = await handler.handle(request, {
+    const rpcResult = await rpcHandler.handle(request, {
         prefix: '/rpc',
         context,
     });
 
-    if (matched) return response;
+    if (rpcResult.matched) return rpcResult.response;
+
+    const apiResult = await apiHandler.handle(request, {
+        prefix: '/rpc/api',
+        context: context,
+    });
+
+    if (apiResult.matched) return apiResult.response;
 });
