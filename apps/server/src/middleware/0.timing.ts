d854a2dce70745493dbe619fa8a47df8b5a4f3b3
x3noku <x3noku@gmail.com>
2026-01-24 22:00:36 +0600

    chore(server): update timing middleware

--- apps/server/src/middleware/0.timing.ts
+++ apps/server/src/middleware/0.timing.ts
@@ -1,20 +1,21 @@
+import { logger } from '@xenous/logger';
 import { defineMiddleware } from 'nitro/h3';
 import { env } from '~/env';
 
 export default defineMiddleware(async (event, next) => {
+    if (env.NODE_ENV !== 'development') {
+        return await next();
+    }
+
     const start = Date.now();
 
-    if (env.NODE_ENV === 'development') {
-        const waitMs = Math.floor(Math.random() * 400) + 100;
-        await new Promise(resolve => setTimeout(resolve, waitMs));
-    }
+    const waitMs = Math.floor(Math.random() * 400) + 100;
+    await new Promise(resolve => setTimeout(resolve, waitMs));
 
     const result = await next();
 
-    if (env.NODE_ENV === 'development') {
-        const end = Date.now();
-        console.log(`[H3] ${event.url.toString()} took ${end - start}ms to execute`);
-    }
+    const end = Date.now();
+    logger.withTag('H3').debug(`${event.req.method} ${event.url.toString()} took ${end - start}ms to execute`);
 
     return result;
 });
