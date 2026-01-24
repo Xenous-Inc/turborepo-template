1ca766281f01da3dfe4f906b89c3c22f624852a6
x3noku <x3noku@gmail.com>
2026-01-24 23:39:52 +0600

    chore(tanstack): add auth utils

--- apps/tanstack/src/shared/lib/auth.ts
+++ apps/tanstack/src/shared/lib/auth.ts
@@ -1,6 +1,26 @@
+import { createMiddleware, createServerFn } from '@tanstack/react-start';
 import { createAuthClient } from 'better-auth/react';
 import { env } from '~/env';
 
 export const authClient = createAuthClient({
     baseURL: env.VITE_SERVER_URL,
 });
+
+export const authMiddleware = createMiddleware().server(async ({ next, request }) => {
+    const session = await authClient.getSession({
+        fetchOptions: {
+            headers: request.headers,
+            throw: true,
+        },
+    });
+
+    return next({
+        context: { session },
+    });
+});
+
+export const getSession = createServerFn({ method: 'GET' })
+    .middleware([authMiddleware])
+    .handler(async ({ context }) => {
+        return context.session;
+    });
