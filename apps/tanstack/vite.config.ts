b35cd86cd39af1e5210f8a81b18c879759b39016
x3noku <x3noku@gmail.com>
2026-04-03 04:16:50 +0300

    chore: update `vite` version to 8

--- apps/tanstack/vite.config.ts
+++ apps/tanstack/vite.config.ts
@@ -4,7 +4,6 @@ import viteReact from '@vitejs/plugin-react';
 import { createJiti } from 'jiti';
 import { nitro } from 'nitro/vite';
 import { defineConfig } from 'vite';
-import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 /**
  * Import env files to validate at build time. Use jiti so we can load .ts files in here.
@@ -15,7 +14,6 @@ await createJiti(import.meta.url).import('./env');
 
 const config = defineConfig({
     plugins: [
-        viteTsConfigPaths({ projects: ['./tsconfig.json'] }),
         nitro({
             preset: 'node_server',
             builder: 'rolldown',
@@ -25,6 +23,9 @@ const config = defineConfig({
         tanstackStart({ router: { routesDirectory: 'app', indexToken: 'page' } }),
         viteReact(),
     ],
+    resolve: {
+        tsconfigPaths: true,
+    },
 });
 
 export default config;
